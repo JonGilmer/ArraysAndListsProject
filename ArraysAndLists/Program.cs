8b21fbe6ca88988eed368913dbe9708951577983
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-17 15:36:51 -0500

    Pushed completed version of Arrays and Lists Exercise 2 Project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -6,27 +7,41 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
-            //TODO:
+            var numArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            // Create an int Array and populate numbers 1-10
+            var evens = new List<int>();
+            var odds = new List<int>();
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            foreach (var number in numArray)
+            {
+                if (number % 2 == 0)
+                {
+                    evens.Add(number);
+                }
+                else
+                {
+                    odds.Add(number);
+                }
+            }
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
+            Console.WriteLine("Evens List: ");
 
-            /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            for (int i = 0; i < evens.Count; i++)
+            {
+                Console.WriteLine(evens[i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Odds List: ");
+
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine(odds[i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
